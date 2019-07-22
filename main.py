153287e0e0bb0645d014d0935976dde03dd52f95
YA <YA-androidapp@users.noreply.github.com>
2019-07-23 08:54:22 +0900

    Add haarcascade files

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # $  pip install icrawler
 
 from icrawler.builtin import GoogleImageCrawler
+import cv2
 import datetime
 import glob
 import os
@@ -14,13 +15,28 @@ import re
 
 DATAFILEPATH = 'q.txt'
 IMAGEDIRPATH = 'images'
+FACEIMAGEDIRPATH = 'images_face'
+
+
+HAARCASCADE_PATH = 'sources/data/haarcascades/haarcascade_frontalface_default.xml'
+
+
+IMAGEDIRPATH = IMAGEDIRPATH if IMAGEDIRPATH.endswith(os.path.sep) else (IMAGEDIRPATH + os.path.sep)
+FACEIMAGEDIRPATH = FACEIMAGEDIRPATH if FACEIMAGEDIRPATH.endswith(os.path.sep) else (FACEIMAGEDIRPATH + os.path.sep)
 
 
 def checkDir():
+    # IMAGEDIRPATH
     if os.path.exists(IMAGEDIRPATH):
         nowstr = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        os.rename(IMAGEDIRPATH, IMAGEDIRPATH + '_' + nowstr + '.bak')
+        os.rename(os.path.dirname(IMAGEDIRPATH), os.path.dirname(IMAGEDIRPATH) + '_' + nowstr + '.bak')
     os.makedirs(IMAGEDIRPATH, exist_ok=True)
+
+    # FACEIMAGEDIRPATH
+    if os.path.exists(FACEIMAGEDIRPATH):
+        nowstr = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        os.rename(os.path.dirname(FACEIMAGEDIRPATH), os.path.dirname(FACEIMAGEDIRPATH) + '_' + nowstr + '.bak')
+    os.makedirs(FACEIMAGEDIRPATH, exist_ok=True)
 
 
 def collect():
@@ -38,12 +54,34 @@ def collect():
 
 
 def filter():
-    # TODO: Haar cascadeを使用して、顔画像が含まれるファイルのみ残す
+    faceCascadeClassifier = cv2.CascadeClassifier(HAARCASCADE_PATH)
+
     dirs = glob.glob(os.path.join(IMAGEDIRPATH, '**' + os.sep), recursive=True)
-    print(dirs)
+    # print(dirs) # ['images/', 'images/AAA/', 'images/BBB/', 'images/CCC/', 'images/DDD/', 'images/EEE/']
+    for dir in dirs:
+        if os.path.basename(os.path.dirname(dir)) == os.path.basename(os.path.dirname(IMAGEDIRPATH)):
+            continue
+        subdir = os.path.basename(os.path.dirname(dir))
+        print('subdir: ' + subdir)
+
+        # 出力先
+        mvdir = os.path.join(FACEIMAGEDIRPATH, subdir)
+        os.makedirs(mvdir, exist_ok=True)
+
+        # 移動し得るファイルを検索
+        files = glob.glob(os.path.join(dir, '?*.???*'), recursive=True) # 拡張子3文字以上のファイル
+        # print(files) # ['images/EEE/000001.jpg', 'images/EEE/000002.jpg', 'images/EEE/000003.jpg', 'images/EEE/000173.jpg']]
+        for file in files:
+            #TODO: ファイル内容が画像か判定
+            detectFace(file)
+
+
+def detectFace(file):
+    # TODO: Haar cascadeを使用して、顔領域を切り出して結果出力用ディレクトリに保存する
+    pass
 
 
 if __name__ == '__main__':
-    checkDir()
-    collect()
+    # checkDir()
+    # collect()
     filter()
