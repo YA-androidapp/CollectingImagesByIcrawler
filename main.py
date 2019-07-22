3d113cd76b89b10580f47503d1833dea7e5a978e
YA <YA-androidapp@users.noreply.github.com>
2019-07-22 12:56:47 +0900

    Add filter()

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 from icrawler.builtin import GoogleImageCrawler
 import datetime
+import glob
 import os
 import re
 
@@ -15,12 +16,14 @@ DATAFILEPATH = 'q.txt'
 IMAGEDIRPATH = 'images'
 
 
-def main():
+def checkDir():
     if os.path.exists(IMAGEDIRPATH):
         nowstr = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
         os.rename(IMAGEDIRPATH, IMAGEDIRPATH + '_' + nowstr + '.bak')
     os.makedirs(IMAGEDIRPATH, exist_ok=True)
 
+
+def collect():
     with open(DATAFILEPATH) as f:
         for line in f:
             keyword = line.strip().replace(' ', '').replace('　', '')
@@ -34,5 +37,13 @@ def main():
             crawler.crawl(keyword=keyword, max_num=200)
 
 
+def filter():
+    # TODO: Haar cascadeを使用して、顔画像が含まれるファイルのみ残す
+    dirs = glob.glob(os.path.join(IMAGEDIRPATH, '**' + os.sep), recursive=True)
+    print(dirs)
+
+
 if __name__ == '__main__':
-    main()
+    checkDir()
+    collect()
+    filter()
